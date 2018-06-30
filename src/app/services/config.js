d098f0bb95f39ecc140e386e68f6d5f37834f964
smallwaterdev <hatakusunoki@qwqwqwq.local>
2018-06-30 23:22:53 +0800

    meta update done

--- src/app/services/config.js
+++ src/app/services/config.js
@@ -1,1 +1,2 @@
-export const smallData_manager_addr = "http://128.110.153.109:3001";
+// export const smallData_manager_addr = "http://128.110.153.109:3001";
+export const smallData_manager_addr = "http://192.168.1.100:3001";
